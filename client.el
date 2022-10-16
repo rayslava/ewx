03f37cd6b49ec26aeb8d85505c76ddc989c0cff2
Michael Bauer <michael-bauer@posteo.de>
2022-10-16 22:40:00 +0200

    ewc: Sketch elisp wayland client implementation

--- client.el
+++ client.el
@@ -77,3 +77,43 @@
 ;;  (length . 28)
 ;;  (opcode . 0)
 ;;  (id . 2))
+
+;;; impl: ewc.el a wayland client in elisp:)
+(defvar ewc-header
+  '((id u32r)
+    (opcode u16r)
+    (length u16r)))
+
+;; parse
+(bindat-unpack ewc-header str idx)
+-> id opcode length
+(bindat-unpack (lookup msg-spec id opcode) str 4)
+idx = (+ idx length) | finished if = (length str) ; make rest an error for now
+
+(defvar ewc-objects nil
+  ;; "Alist object ID to object name in current session."
+  "List of objects (name) in current session.") ; Use nconc to add new?
+;; Next two are seeded from protocol xml
+(defvar ewc-events nil
+  "Alist object name to list/vector of event data spec")
+(defvar ewc-requests nil
+  "Alist object name to list/vector of request data spec")
+(defvar ewc-callbacks|listeners nil
+  "Alist object name to list/vector of event callbacks")
+
+(defun ewc-parse (str str-len idx)
+  ;; header
+  (pcase-let (((map id opcode len) (bindat-unpack ewc-header str idx)))
+    (bindat-unpack
+     (seq-elt (map-elt ewc-events
+                       (seq-elt ewc-objects id))
+              opcode)
+     str
+     (+ idx 8))  ; -> call callback
+    (let ((idx (+ idx len)))
+      (unless (= str-len idx)
+        (ewc-parse str str-len idx)))))
+
+;; filter
+(lambda (proc str)
+  (ewc-parse str (length str) 0))
