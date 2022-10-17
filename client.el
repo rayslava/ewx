281c63fbc86f5b92ba1c474820194ee088f2fc3e
Michael Bauer <michael-bauer@posteo.de>
2022-10-17 12:13:55 +0200

    ewc: Add reader for wayland xml protocol files (~ wayland-scanner)

--- client.el
+++ client.el
@@ -94,11 +94,11 @@ idx = (+ idx length) | finished if = (length str) ; make rest an error for now
 (defvar ewc-objects nil
   ;; "Alist object ID to object name in current session."
   "List of objects (name) in current session.") ; Use nconc to add new?
-;; Next two are seeded from protocol xml
-(defvar ewc-events nil
-  "Alist object name to list/vector of event data spec")
-(defvar ewc-requests nil
-  "Alist object name to list/vector of request data spec")
+;; Next is seeded from protocol xml
+(defvar ewc-protocols nil
+  "
+Tree protocol-name->interface-name->events->((event-name . bindat-spec) ...)
+                                  ->requests->((request-name . bindat-spec) ...)")
 (defvar ewc-callbacks|listeners nil
   "Alist object name to list/vector of event callbacks")
 
@@ -118,3 +118,76 @@ idx = (+ idx length) | finished if = (length str) ; make rest an error for now
 ;; filter
 (lambda (proc str)
   (ewc-parse str (length str) 0))
+
+;; ewc-protocols
+;; (protocol-name
+;;  (interface-name
+;;   (events
+;;    (event-name . spec)
+;;    ...)
+;;   (requests
+;;    (request-name . spec)
+;;    ...))
+;;  ...)
+
+(defun ewc-protocol-read (xml-protocol-file)
+  ;; This is wayland-scanner
+  (pcase-let* ((node (with-current-buffer (find-file-noselect xml-protocol-file)
+                       (libxml-parse-xml-region (point-min) (point-max))))
+               (`(,protocol . ,interfaces) (ewc-protocol node)))
+    (setf (map-elt ewc-protocols protocol) interfaces)))
+
+(ewc-protocol-read "~/s/wayland/ref/wayland/protocol/wayland.xml")
+(ewc-protocol-read "~/s/wayland/ewp.xml")
+;; =>
+;; (emacs_wayland_protocol
+;;  (layout
+;;   (events
+;;    (layout-new-window
+;;     (current_output u32r)
+;;     (usable_width u32r)
+;;     (usable_height u32r)
+;;     (tags u32r)
+;;     (title u32r)
+;;     (application u32r)
+;;     (pid u32r)))
+;;   (requests
+;;    (layout-window
+;;     (x u32r)
+;;     (y u32r)
+;;     (width u32r)
+;;     (height u32r)))))
+
+(defsubst ewc-node-name (node)
+  (intern (dom-attr node 'name)))
+
+(defun ewc-protocol (node)
+  ;; (protocol interface... ...)
+  (cons (ewc-node-name node)
+        (mapcar #'ewc-protocol-interface (dom-by-tag node 'interface))))
+
+(defun ewc-protocol-interface (node)
+  ;; (interface event... request... ...)
+  (list (ewc-node-name node)
+        (cons 'events
+              (mapcar #'ewc-protocol-msg (dom-by-tag node 'event)))
+        (cons 'requests
+              (mapcar #'ewc-protocol-msg (dom-by-tag node 'request)))))
+
+(defun ewc-protocol-msg (node)
+  ;; (event | request name arg ...)
+  (cons (ewc-node-name node)
+        (mapcan #'ewc-protocol-arg (dom-by-tag node 'arg))))
+
+(defun ewc-protocol-arg (node)
+  ;; (arg name type ...)
+  (let ((name (ewc-node-name node)))
+    (pcase (dom-attr node 'type)
+      ((or "uint" "object" "new_id")
+       `((,name u32r)))
+      ("string"
+       `((str-len u32r)
+         (,name strz (str-len))
+         (align 4)))
+      ((or "int" "fixed" "array" "fd" "enum")
+       `((,name not-implemented))))))
