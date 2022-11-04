38fa29c7bb3323034b0cdd76dcab8b69022ebeda
Michael Bauer <michael-bauer@posteo.de>
2022-11-04 19:42:22 +0100

    ewc: Make test use hash table ewc-objects, new-layout and layout

--- ewc-test.el
+++ ewc-test.el
@@ -1,12 +1,11 @@
 ;;; Test  -*- lexical-binding: t; -*-
 ;; Experimentar
-(setq ewc-protocols
-      (ewc-read "~/s/wayland/ref/wayland/protocol/wayland.xml"
-                "~/s/wayland/ewp.xml"))
+;; (setq ewc-protocols
+;;       (ewc-read "~/s/wayland/ref/wayland/protocol/wayland.xml"
+;;                 "~/s/wayland/ewp.xml"))
 
 ;; Reset
 (delete-process (ewc-object-data (nth 0 ewc-objects)))
-(setq ewc-objects nil)
 
 ;; Setup
 (ewc-connect)
@@ -17,10 +16,9 @@ ewc-objects
 
 (ewc-get-registry)
 
-(ewc-object-data (nth 1 ewc-objects))
+(ewc-object-data (gethash 2 (ewc-objects-table ewc-objects)))
 ;; =>
-;; ((13 (interface . "wl_output") (version . 4))
-;; (12 (interface . "wl_output") (version . 4))
+;; ((12 (interface . "wl_output") (version . 4))
 ;; (11 (interface . "ewp_layout") (version . 1))
 ;; (10 (interface . "wl_seat") (version . 8))
 ;; (9 (interface . "org_kde_kwin_server_decoration_manager") (version . 1))
@@ -37,18 +35,37 @@ ewc-objects
 ;; or this is ewc-XXX-bind = bind an object from the registry
 (ewc-object 'emacs-wayland-protocol 'ewp-layout nil)
 
-(ewc-request (nth 1 ewc-objects) 'bind `((name . 11)
-                                         (interface-len . ,(1+ (length "ewp_layout")))
-                                         (interface . "ewp_layout")
-                                         (version . 1)
-                                         (id . 3)))
+(ewc-request (gethash 2 (ewc-objects-table ewc-objects))
+             'bind `((name . 11)
+                     (interface-len . ,(1+ (length "ewp_layout")))
+                     (interface . "ewp_layout")
+                     (version . 1)
+                     (id . 3)))
 
-(ewc-request (nth 2 ewc-objects) 'layout-window '((x . 0)
-                                                  (y . 0)
-                                                  (width . 0)
-                                                  (height . 0)))
+;; (ewc-request (nth 2 ewc-objects) 'layout-window '((x . 0)
+;;                                                   (y . 0)
+;;                                                   (width . 0)
+;;                                                   (height . 0)))
 
 
+(setf (ewc-listener (gethash 3 (ewc-objects-table ewc-objects)) 'new-surface)
+      (lambda (object new-object)
+        ;; new-object = (map id app-id title pid)
+        (message "New ewp-surface: %s" new-object)
+        (ewc-object 'emacs-wayland-protocol 'ewp-surface new-object
+                    (alist-get 'id new-object))))
+
+(ewc-request (gethash 4278190080 (ewc-objects-table ewc-objects))
+             'layout '((x . 20)
+                       (y . 20)
+                       (width . 800)
+                       (height . 400)))
+
+(ewc-request (gethash 4278190080 (ewc-objects-table ewc-objects))
+             'layout '((x . 50)
+                       (y . 50)
+                       (width . 200)
+                       (height . 400)))
 
 ;;; Test wl_output
 (ewc-object 'wayland 'wl-output nil)
