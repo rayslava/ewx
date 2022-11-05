f8e8a8d968c3cac3a6ea170de565fc272f18052a
Michael Bauer <michael-bauer@posteo.de>
2022-11-05 18:59:32 +0100

    ewc: Restructure ewc-protocols & -objects dyn vars to arguments
    ewc-protocols was needed for ewc-object
    ewc-objects was needed for ewc-object, ewc-event & ewc-connect
    
    Instead
    -> Make protocols part of ewc-objects struct
    -> Pass ewc-objects struct to ewc-object-add (Renamed from ewc-object)
    -> ewc-connect returns an ewc-objects struct and consumes ewc-protocols
    
    Protocols and objects should be linked because objects that share the
    same protocols share the same listeners.
    
    This restructure allows for a more encapsulated client design that
    does not rely on global dynamic state.

--- ewc.el
+++ ewc.el
@@ -35,6 +35,15 @@
 
 ;; Seems sound: short & concise :)
 
+;; Things:
+;; protocols, objects, wayland wire messages, helper
+
+;; Objects created from the same protocols share the same listeners.
+;; ewc-objects is the runtime struct that contains
+;; - the list of objects
+;; - the next new-id
+;; - the protocols in use
+
 ;;; Code:
 (require 'cl-macs)
 (require 'seq)
@@ -135,12 +144,10 @@ This is the elisp version of wayland-scanner."
        ;; Use https://github.com/skeeto/bitpack ?
        `((,name not-implemented))))))
 
-(defvar ewc-protocols nil "")
-
 ;;; Objects
 ;; implementing the wayland interfaces.
 
-(cl-defstruct (ewc-object (:constructor ewc-object--make)
+(cl-defstruct (ewc-object (:constructor ewc-object-make)
                           (:copier nil))
   "A client side object implementing a wayland interface."
   (protocol nil :type symbol :read-only t)
@@ -155,34 +162,49 @@ This is the elisp version of wayland-scanner."
                            (:copier nil))
   "All objects in the current session.
 new-id is the id of the next client initiated object.
-table is a hash table from object id to `ewc-object'."
+table is a hash table from object id to `ewc-object'.
+protocols is an alist from `ewc-read'."
   (new-id 1 :type integer)
-  (table (make-hash-table) :type hash-table :read-only t))
+  (table (make-hash-table) :type hash-table :read-only t)
+  (protocols nil :type list :read-only t))
 
-(defvar ewc-objects nil
-  "Holds the `ewc-objects' of the current session.")
+(define-inline ewc-object-get (id objects)
+  "Get object with ID from OBJECTS an `ewc-objects' struct."
+  (inline-quote (gethash ,id (ewc-objects-table ,objects))))
 
-(defun ewc-object (protocol interface &optional data id)
-  "Generate an object from INTERFACE of PROTOCOL with optional DATA.
-Use optional ID for server initiated objects."
-  (pcase-let* ((`(,_version ,events ,requests)
+(defun ewc-object-add (&rest args)
+  "Add a new object implementing INTERFACE of PROTOCOL to OBJECTS
+with optional ID or DATA.
+
+Returns the newly created object.
+
+OBJECTS is an `ewc-objects' struct.
+PROTOCOL and INTERFACE are symbols.
+DATA can be any value and ID is uint32.
+
+Use optional ID for server initiated objects.
+
+\(fn &key OBJECTS PROTOCOL INTERFACE ID DATA)"
+  (pcase-let* (((map :objects :protocol :interface :id :data)
+                args)
+               (`(,_version ,events ,requests)
                 (thread-last
-                  ewc-protocols
+                  (ewc-objects-protocols objects)
                   (alist-get protocol)
                   (alist-get interface)))
-               (id (or id (ewc-objects-new-id ewc-objects)))
-               (object (ewc-object--make
+               (id (or id (ewc-objects-new-id objects)))
+               (object (ewc-object-make
                         :protocol protocol
                         :interface interface
                         :id id
                         :data data
                         :events events
                         :requests requests)))
-    (puthash id object (ewc-objects-table ewc-objects))
-    (cl-incf (ewc-objects-new-id ewc-objects))
+    (puthash id object (ewc-objects-table objects))
+    (cl-incf (ewc-objects-new-id objects))
     object))
 
-;; This sets the listener for all objects.
+;; This sets the listener for all objects created from the same protocols.
 ;; define-inline makes ewc-listener usable with setf.
 (define-inline ewc-listener (object event)
   "Return listener for event."
@@ -195,14 +217,14 @@ Use optional ID for server initiated objects."
                (opcode uint 16 t)
                (len uint 16 t)))
 
-(defun ewc-event (str str-len idx)
-  "Parse wayland event wire message STRing with STR-LENength starting at IDX
-and dispatch to the events listener."
+(defun ewc-event (objects str str-len idx)
+  "Parse wayland event wire message STRing with STR-LENgth starting at IDX.
+Lookup the object id in OBJECTS and dispatch to the events listener."
   (pcase-let*
       ((bindat-idx idx)
        (bindat-raw str)
        ((map id opcode _len) (funcall (bindat--type-ue ewc-msg-head)))
-       (object (gethash id (ewc-objects-table ewc-objects)))
+       (object (ewc-object-get id objects))
        (`(,_event ,ue . ,listener) (nth opcode (ewc-object-events object))))
 
     ;; DEBUG
@@ -215,7 +237,7 @@ and dispatch to the events listener."
              object (when ue (funcall ue)))
     
     (unless (= str-len bindat-idx)
-      (ewc-event bindat-raw str-len bindat-idx))))
+      (ewc-event objects bindat-raw str-len bindat-idx))))
 
 (defun ewc-pack (object request arguments)
   "Return wayland REQUEST wire message for OBJECT with ARGUMENTS."
@@ -236,38 +258,51 @@ and dispatch to the events listener."
     bindat-raw))
 
 ;;; wl-display
-(defun ewc-filter (_proc str)
-  ;; DEBUG
-  (with-current-buffer (get-buffer-create "*wayland*")
-    (insert str))
+(defun ewc-filter (objects)
+  (lambda (_proc str)
+    ;; DEBUG
+    (with-current-buffer (get-buffer-create "*wayland*")
+      (insert str))
 
-  (ewc-event str (length str) 0))
+    (ewc-event objects str (length str) 0)))
 
-(define-inline ewc-display ()
-  (inline-quote (gethash 1 (ewc-objects-table ewc-objects))))
-
-(defun ewc-connect ()
+(defun ewc-connect (protocols &optional socket)
+  "Connect to wayland SOCKET using PROTOCOLS.
+SOCKET defaults to the value of WAYLAND_DISPLAY.
+Returns a `ewc-objects' struct with wl-display as object 1."
   ;; | ewc-init
-  (if (and ewc-objects
-           (process-live-p (ewc-object-data (ewc-display))))
-      (message "Emacs wayland client already connected")
-    (setq ewc-objects (ewc-objects-make))
-    (ewc-object  'wayland 'wl-display
-                 (make-network-process
-                  :name "emacs-wayland-client"
-                  :remote "/run/user/1000/wayland-0" ; TODO: Make configurable/ detect.
-                  :coding 'binary
-                  :filter #'ewc-filter))
-    (message "Emacs wayland client connected")))
+  (let ((objects (ewc-objects-make :protocols protocols)))
+    (ewc-object-add
+     :objects objects
+     :protocol 'wayland
+     :interface 'wl-display
+     :data (make-network-process
+            :name "emacs-wayland-client"
+            :remote (or socket (getenv "WAYLAND_DISPLAY"))
+            :coding 'binary
+            :filter (ewc-filter objects)))
+    objects))
 
-(defun ewc-request (object request arguments)
-  (process-send-string (ewc-object-data (ewc-display)) (ewc-pack object request arguments)))
+(defun ewc-request (objects object request arguments)
+  "Issue REQUEST with ARGUMENTS of OBJECT
+OBJECT can be an `ewc-object' struct or an id.
+OBJECTS is an `ewc-objects' struct."
+  (process-send-string (ewc-object-get 1 objects)
+                       (ewc-pack
+                        (cond
+                         ((ewc-object-p object) object)
+                         ((natnump object) (ewc-object-get object objects))
+                         (t (error "Wrong object %s" object)))
+                        request
+                        arguments)))
 
 ;; TODO: Add cleanup fn
 
 ;;; wl-registry
-(defun ewc-get-registry ()
-  (let ((registry (ewc-object 'wayland 'wl-registry)))
+(defun ewc-get-registry (objects)
+  (let ((registry (ewc-object-add :objects objects
+                                  :protocol 'wayland
+                                  :interface 'wl-registry)))
     ;; Add global listener
     (setf (ewc-listener registry 'global)
           (pcase-lambda (object (map name interface version))
@@ -275,13 +310,8 @@ and dispatch to the events listener."
             (push `(,name (interface . ,interface) (version . ,version))
                   (ewc-object-data object))))
     ;; Issue the request
-    (ewc-request (ewc-display) 'get-registry `((registry . ,(ewc-object-id registry))))
+    (ewc-request objects 1 'get-registry `((registry . ,(ewc-object-id registry))))
     registry))
 
 (provide 'ewc)
 ;;; ewc.el ends here
-
-;; Must be here to work atm. 
-(setq ewc-protocols
-      (ewc-read "~/s/wayland/ref/wayland/protocol/wayland.xml"
-                "~/s/wayland/ewp.xml"))
